87a6510c4b58bdac66c3d380be2e5f8e50e4ad4a
brian-ikiara <brnikiara@gmail.com>
2022-10-14 12:51:21 +0000

    Puts what?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+ * the main function marks the beginning and end of the program.
+ * It's where functions, that're defined beforehand, are called  
+ */
+
 int main()
 {
 	puts("Programming is like building a multilingual puzzle");
