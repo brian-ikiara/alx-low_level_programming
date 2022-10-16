f59d2b4390b3454bda6a2f6b73173f3e35d2904c
brian-ikiara <brnikiara@gmail.com>
2022-10-16 17:56:31 +0000

    Combinatronics

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Beginning of program
+ * Description: Combinatronics.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	j = i % 10
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(j + '0');
+		if (num == 9)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
