f8627d84b4c56f7b0596a7a7c27cf96b2a23ed5f
brian-ikiara <brnikiara@gmail.com>
2022-10-17 13:27:29 +0000

    Software gore!

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,20 +12,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 99; j++)
 		{
 			if (i < j)
 			{
-				
 				putchar((i / 10) + '0');
 				putchar((j % 10) + '0');
 				putchar(' ');
 				putchar((i % 10) + '0');
 				putchar((j / 10) + '0');
 
-				if (i != 8 || (i == 8 && j != 9))
+				if (i != 98 || (i == 98 && j != 99))
 				{
 					putchar(',');
 					putchar(' ');
