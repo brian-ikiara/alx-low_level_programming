b4f87c233b8ef91bed90e22bf68523500769461a
brian-ikiara <brnikiara@gmail.com>
2022-10-18 09:39:47 +0000

    Ah! Here we go again!

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  * main - Beginning of program
  *
  * Description: Prints "_putchar".
- * Return: 0.
+ * Return: 1.
  */
 
 int main(void)
 {
 	_putchar('_');
 	printf("putchar\n");
-	return (0);
+	return (1);
 }
