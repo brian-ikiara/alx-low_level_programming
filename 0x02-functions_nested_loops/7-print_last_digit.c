c07d1c2c1430b3f19bdf95ad5bb4c9d4c1d6ed01
brian-ikiara <brnikiara@gmail.com>
2022-10-18 13:23:28 +0000

    Last digit standing!

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function prototype
+ * @n: Number to be checked
+ *
+ * Description: Prints the value of
+ * the last digit of a given number.
+ * Return: n % 10.
+ * On error, stderr.
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
