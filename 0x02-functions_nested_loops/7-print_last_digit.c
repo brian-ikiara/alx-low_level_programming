091429139a9f6332fe3c2a3bd8c667db13b29ddc
brian-ikiara <brnikiara@gmail.com>
2022-10-18 13:34:05 +0000

    Last digit standing!

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,10 +17,10 @@ int print_last_digit(int n)
 	/* Converts n to absolute value if n is neg. */
 	if (n < 0)
 	{
-		ldig = ldig * -1; 
+		ldig = ldig * -1;
 	}
 	/* Repeats return value to please checker */
 	_putchar(ldig + '0');
 
-	return (n % 10);
+	return (n);
 }
