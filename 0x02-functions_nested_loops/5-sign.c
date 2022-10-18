99e518e64454aa2ef8e7e50851d12c2aa9c2edcb
brian-ikiara <brnikiara@gmail.com>
2022-10-18 12:43:21 +0000

    It's a miracle!

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,13 +15,13 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	else
 		return (0);
