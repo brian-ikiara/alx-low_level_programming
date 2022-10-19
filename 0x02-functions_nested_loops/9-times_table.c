06c49d3c0fdf01821fea3c9c318be7dc93fc5925
brian-ikiara <brnikiara@gmail.com>
2022-10-19 11:53:33 +0000

    Tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
  /**
   * times_table - Function prototype
-  *
   * Description: Prints a literal times table
   * of 9.
+  *
   * Return: 0, since it's a void return type.
   * On error, stderr.
   */
