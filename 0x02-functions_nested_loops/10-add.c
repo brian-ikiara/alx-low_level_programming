0713cbcbd0bc07fc1a6f6d1e998b7e8ff3bf71b8
brian-ikiara <brnikiara@gmail.com>
2022-10-19 12:04:17 +0000

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Function prototype
+ * @a: First digit to be added
+ * @b: Second digit to be added
+ *
+ * Description: Adds to digits together
+ * and prints out result.
+ * Return: a + b.
+ * On error, stderr.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
