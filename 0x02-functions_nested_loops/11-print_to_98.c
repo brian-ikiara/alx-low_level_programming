b3f7b226ea92f5efa25b030725a3433e2f4d14d4
brian-ikiara <brnikiara@gmail.com>
2022-10-19 12:49:58 +0000

    1998

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Function prototype
+ * @n: Number to be checked
+ *
+ * Description: Returns every number the number
+ * n is away from 98.
+ * Return: 0, since it's the void data type
+ * On error, stderr.
+ */
+
+void print_to_98(int n)
+{
+	/** I had to use the pfintf function
+	 * as opposed to the _putchar, since it:
+	 * a) Makes work way easier
+	 * b) You don't have to type '0' after an integer
+	 * since it references it using %d
+	 * c) Enables the statement(s), "n--/n++"
+	 */
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			/* Prints positive numbers */
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			/* Prints negative numbers */
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
