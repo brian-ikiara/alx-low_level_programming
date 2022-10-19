3ab6a30de07dca773d5d4866c858ab3321da377c
brian-ikiara <brnikiara@gmail.com>
2022-10-19 11:17:57 +0000

    Tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,4 +5,40 @@
   * Description: Prints a literal times table
   * of 9.
   * Return: 0, since it's a void return type.
-  *:
+  * On error, stderr.
+  */
+
+void times_table(void)
+{
+	int x = 0, y = 0, z;
+
+	while (x <= 9)
+	{
+		while (y <= 9)
+		{
+			z = x * y;
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			}
+
+			if (z <= 9 && y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			y++;
+		}
+		putchar('\n');
+		x++;
+	}
+}
