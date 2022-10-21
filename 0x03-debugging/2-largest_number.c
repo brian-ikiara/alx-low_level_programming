f34f2ed9ad22af737b4552bf1b6e8f4e8491ebd1
brian-ikiara <brnikiara@gmail.com>
2022-10-21 08:50:10 +0000

    He protec

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,11 +20,14 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
+	else if (a == b && a == c)
+	{
+		largest = a;
+	}
 
 	return (largest);
-	return (0);
 }
