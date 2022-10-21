65b556b7d9e929f76913175a9f83b4009e7157ff
brian-ikiara <brnikiara@gmail.com>
2022-10-21 10:49:39 +0000

    Mul is dull

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - Function prototype
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Description: Gets product of two integers.
+ * Return: result, of the multiplication.
+ * On error, stderr.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
