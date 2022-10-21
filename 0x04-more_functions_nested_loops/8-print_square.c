569d4009cc670795579a2db28afdaa845a9452b1
brian-ikiara <brnikiara@gmail.com>
2022-10-21 18:31:58 +0000

    Push Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,12 +14,17 @@ void print_square(int size)
 {
 	int i, j, size;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (i = 0; i < size; i++)
+		_putchar('\n');
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar(
+			_putchar('#')
+		}
+		_putchar('\n');
+	}
+}
