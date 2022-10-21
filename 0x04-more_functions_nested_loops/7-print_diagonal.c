8fa20d8b96beb4537662ebe80b48f247f1a3f120
brian-ikiara <brnikiara@gmail.com>
2022-10-21 16:53:13 +0000

    Not parallel parked?

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,11 +12,13 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n > 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (int j = 0; j < n; j++)
+			for (j = 0; j < n; j++)
 			{
 				if (j == i)
 				{
