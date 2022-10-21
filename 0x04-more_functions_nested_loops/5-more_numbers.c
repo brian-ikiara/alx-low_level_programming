ac98eb4db57971f56175df737427150946f9ce29
brian-ikiara <brnikiara@gmail.com>
2022-10-21 12:26:42 +0000

    More numbers please!

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function prototype
+ *
+ * Description: Prints numbers 0 to 14 on
+ * 10 lines.
+ * Return: 0, since it's void.
+ * On error, stderr.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
