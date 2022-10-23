d0943b171e123b47f2a277eb5401b69bed6f012b
brian-ikiara <brnikiara@gmail.com>
2022-10-23 14:31:59 +0000

    Fizz Buzz, said the fly

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,11 +3,7 @@
 /**
  * main - Entry point of program
  *
- * Description: Prints "Fizz" for a multiple of 3 and
- * "Buzz" for a multiple of 5; "FizzBuzz" for a multiple
- * of both 3 and 5.
  * Return: 0, as always.
- * On error, stderr.
  */
 
 int main(void)
