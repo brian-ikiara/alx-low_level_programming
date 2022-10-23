6e719a676ecce7bef3327d010e12ca36a0a4b3ef
brian-ikiara <brnikiara@gmail.com>
2022-10-23 15:23:42 +0000

    Optimus Prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Description: Prints the prime factors of any
+ * number.
+ * Return: 0, as always.
+ * On error, stderr.
+ */
+
+int main(void)
+{
+	int n = 612852475143;
+
+	while ((n % 2) == 0)
+	{
+		printf("%d ", 2);
+		n /= 2;
+	}
+
+	for (int i = 3; i < sqrt(n); i += 2)
+	{
+		while ((n % i) == 0)
+		{
+			printf("%d ", i);
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+		printf("%d ", n);
+
+	return (0);
+}
