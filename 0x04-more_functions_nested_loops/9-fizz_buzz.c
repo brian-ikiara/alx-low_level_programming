512cc216e3d9bd187644d7b20145d36e7c9a35cd
brian-ikiara <brnikiara@gmail.com>
2022-10-23 14:28:35 +0000

    Fizz Buzz, said the fly

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,29 +14,27 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 0;
+	while (i <= 100)
 	{
-		if ((i % 3) == 0 && (i % 5) == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 5) != 0)
 		{
 			printf("Fizz");
 		}
-		else if ((i % 5) == 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
 			printf("Buzz");
 		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-
-		if (i != 100)
-		{
-			putchar(' ');
-		}
+		putchar(' ');
+		i++;
 	}
 	putchar('\n');
 
