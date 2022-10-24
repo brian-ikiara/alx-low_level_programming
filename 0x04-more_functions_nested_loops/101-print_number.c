837fb2ad0746239726513f86151f8826e8731bed
brian-ikiara <brnikiara@gmail.com>
2022-10-24 13:26:15 +0000

    First encounter with monster Recursion. I shouldn't even say its name

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,14 +12,17 @@
 
 void print_number(int n)
 {
+	unsigned int i;
+
+	i = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		i = n * -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (i / 10)
+		print_number(i / 10);
 
 	_putchar((n % 10) + '0');
 }
