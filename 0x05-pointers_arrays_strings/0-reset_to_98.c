7145131e4ffad6e127adc12c7a45c581f061efa3
brian-ikiara <brnikiara@gmail.com>
2022-10-25 09:52:16 +0000

    1998 was a very GoodYear

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function prototype
+ * @*n: Pointer to address of variable
+ *
+ * Description: Updates the value of the variable it
+ * points to, to 98.
+ * Return: void.
+ * On error, stderr.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+**X gonna point to you!**
