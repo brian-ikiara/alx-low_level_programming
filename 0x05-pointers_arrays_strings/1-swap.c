71328ee652953eb7cf844cbf4d8e3cdc04981808
brian-ikiara <brnikiara@gmail.com>
2022-10-25 10:23:46 +0000

    Swapsees!

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - Function prototype
+ * @a: Number to be swapped
+ * @b: Number a's swapped to
+ *
+ * Description: Swaps the value of 2 integers.
+ * Return: void.
+ * On error, stderr.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = b;
+	*b = c;
+}
