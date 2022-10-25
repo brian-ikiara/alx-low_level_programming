e421d0f3efafd9c195de2d92ca18b412a8287eb6
brian-ikiara <brnikiara@gmail.com>
2022-10-25 13:01:13 +0000

    gnirtS

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Function prototype
+ * @s: String to be reversed
+ *
+ * Description: Reverses a string.
+ * Return: void.
+ * On error, stderr
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, l = 0;
+	char t;
+
+	while (s[i++])
+		l++;
+
+	for (i = l + 1; i >= (l / 2); i--)
+	{
+		t = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = t;
+	}
+}
