0da40fc0365cf4aec855e96011e4f9540f03a41c
brian-ikiara <brnikiara@gmail.com>
2022-10-26 18:03:44 +0000

    This array contains...

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,12 +19,9 @@ void print_array(int *a, int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (i <= (n - 1))
-			{
-				printf("%d", a[i]);
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d", a[i]);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 }
