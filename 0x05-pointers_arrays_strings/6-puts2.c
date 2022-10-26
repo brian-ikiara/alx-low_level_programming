feae8d50709d50eb21e34074780bda67bf236353
brian-ikiara <brnikiara@gmail.com>
2022-10-26 08:54:39 +0000

    gnirtS

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -15,8 +15,12 @@ void puts2(char *str)
 
 	while (str[i] == '\0')
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+		str++;
 	}
 	_putchar('\n');
 }
