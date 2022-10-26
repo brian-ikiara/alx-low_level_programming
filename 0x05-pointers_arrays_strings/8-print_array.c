ed4b1bc05f66136b118bec678bd248dd55376d8a
brian-ikiara <brnikiara@gmail.com>
2022-10-26 17:51:20 +0000

    This array contains...

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Function prototype
+ * @a: Pointer to array
+ * @n: Number of elements in array
+ *
+ * Description: Prints an array of integers.
+ * Return: 0, since it's void.
+ * On error, stderr.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
