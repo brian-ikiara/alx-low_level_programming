ac429cb7822c8a44314a2dc73f05d27b0a4559f2
brian-ikiara <brnikiara@gmail.com>
2022-10-27 11:36:38 +0000

    Puts .5

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -38,12 +38,9 @@ void puts_half(char *str)
 
 	while (str[i] != '\0')
 	{
-		while (n >= (l / 2))
+		while ((n >= (l / 2)) && ((n % 2) != 0))
 		{
-			if ((n % 2) != 0)
-			{
-				_putchar(str[i]);
-			}
+			_putchar(str[i]);
 			n--;
 		}
 		i++;
