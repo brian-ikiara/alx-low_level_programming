c5195b12369ec132c0c33647d70b71079a8cdab5
brian-ikiara <brnikiara@gmail.com>
2022-10-27 17:34:38 +0000

    Nth Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,32 +14,32 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n > 0 || n <= 15)
+	if (n > 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				k = i * j;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
+				_putchar(',');
+				_putchar(' ');
 
-				if (k <= n && j != 0)
+				k = i * j;
+
+				if (k <= 9)
+					_putchar(' ');
+				if (k <= 99)
+					_putchar(' ');
+				if (k >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) % 10 + '0');
 				}
-				else if (k >= (n + 1))
+				else if (k <= 99 && k >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
 				}
+				_putchar((k & 10) + '0');
 			}
 			_putchar('\n');
 		}
