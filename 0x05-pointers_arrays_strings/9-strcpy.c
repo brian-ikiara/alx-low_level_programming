85cdb98bb2b9abcc19122ba0dcbfb3b2198e10b1
brian-ikiara <brnikiara@gmail.com>
2022-10-27 10:07:41 +0000

    This function copies...

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,11 +13,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] = '\0'; i++)
+	while (src[i] = '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
 
