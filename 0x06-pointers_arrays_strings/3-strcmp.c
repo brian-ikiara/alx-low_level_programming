8db35eef9865786a463c7b2fc20c086a82076506
brian-ikiara <brnikiara@gmail.com>
2022-10-28 09:29:43 +0000

    Comparison made

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function prototype
+ * @s1: String comparor
+ * @s2: String comparee
+ *
+ * Description: Compares 2 strings and returns 1 if s1
+ * is comtained in s2, 0 otherwise.
+ * Return: 1 if contained, 0  otherwise.
+ * On error, stderr.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 > s2 || s1 < s2);
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
