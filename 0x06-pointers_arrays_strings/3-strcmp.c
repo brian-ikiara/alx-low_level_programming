2fdd455ef4e849670d31018da08cc390ff8c2e64
brian-ikiara <brnikiara@gmail.com>
2022-10-29 15:59:02 +0000

    toupper(), again, is that you?

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,14 +15,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*s1 != '\0')
+	while (*s1 && (*s1 == *s2))
 	{
-		if (*s1 != *s2)
-			break;
-
 		s1++;
 		s2++;
 	}
 
-	return (*((unsigned char *) s1) - *((unsigned char *) s2));
+	return (*s1 - *s2);
 }
