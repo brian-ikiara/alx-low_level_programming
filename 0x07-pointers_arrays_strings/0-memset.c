9745a34490957d840973b7732bb75e800933f475
brian-ikiara <brnikiara@gmail.com>
2022-11-02 00:18:51 +0000

    memset() implementation

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,15 +14,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-	unsigned char *p = s;
+	register unsigned *p = *s;
 
-	while (n > 0)
+	while (n-- > 0)
 	{
-		*p = b;
-		p++;
-		n--;
+		*p++ = b;
 	}
-
-	return (p);
+	return (s);
 }
