ef164e2ff555f8a8e89d52eb90063afa821ab843
brian-ikiara <brnikiara@gmail.com>
2022-11-02 01:31:41 +0000

    strstr() implementation

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strstr - Function prototype
+ * @haystack: String to be searched
+ * @needle: Search "phrase"
+ *
+ * Description: Finds the first occurence of substring needle
+ * in the string haystack.
+ * Return; (n + i), pointer to first occurence.
+ * On error, stderr.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
