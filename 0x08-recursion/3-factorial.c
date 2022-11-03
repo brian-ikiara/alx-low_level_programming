29e3d5c3011ad0b9c49d233fe02957479579566c
brian-ikiara <brnikiara@gmail.com>
2022-11-03 11:37:09 +0000

    NNN! = 69

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - Function prototype
+ * @n: Number factored
+ *
+ * Description: Returns the factorial of a number n
+ * using recursion.
+ * Return: The factorial.
+ * On error, stderr.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
