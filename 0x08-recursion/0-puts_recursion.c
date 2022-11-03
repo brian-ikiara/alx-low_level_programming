4440cb7aecb264030c43d1a62b3692825467e1ac
brian-ikiara <brnikiara@gmail.com>
2022-11-03 10:50:53 +0000

    puts() alternative

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,14 +12,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	i++;
-	_puts_recursion(++s);
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
+
+	_putchar(*s);
+	_puts_recursion(++s);
 }
