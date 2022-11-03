c6e1f5e26aa705af0e878faa50a6f03130fb92cf
brian-ikiara <brnikiara@gmail.com>
2022-11-03 11:06:25 +0000

    reverse()+

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Funcrion prototype
+ * @s: String pointer
+ *
+ * Description: Prints a string in reverse using _putchar & employing
+ * recursion.
+ * Return: Nothing, since it's void.
+ * On error, stderr.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
