74faac3ea2da660f37dbcb48f6fdb8d3fdaed4b2
brian-ikiara <brnikiara@gmail.com>
2022-11-07 17:33:51 +0300

    Heisenberg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Description: Prints the name of the program.
+ * Return: 0, success.
+ * On error, stderr.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+
+	return (0);
+}
