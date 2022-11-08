1b265bc901049d391d16c341a00243bcdf602686
brian-ikiara <brnikiara@gmail.com>
2022-11-08 09:10:01 +0000

    Numbers

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program's entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of program's symbols
+ *
+ * Description: Prints out the number of command-line arguments.
+ * Return: 0, success.
+ * On error, stderr.
+ */
+
+int main(int __attribute((unused)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
