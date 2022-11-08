74c95a52e556e0744118a4d7d54d97f0b6717016
brian-ikiara <brnikiara@gmail.com>
2022-11-08 09:37:41 +0000

    Numbers

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,9 +10,9 @@
  * On error, stderr.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
