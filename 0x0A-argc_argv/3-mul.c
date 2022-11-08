cb19d75d3872e59615039791d891324019161d91
brian-ikiara <brnikiara@gmail.com>
2022-11-08 10:00:12 +0000

    mul() for main(args)

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Program's entry point
+ * @argc: Number of cmds
+ * @argv: Array of args
+ *
+ * Description: Returns the product of 2 numbers.
+ * Return: 0, success.
+ * On error, 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = atoi(argv[1]), j = atoi(argv[2]), r;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	r = i * j;
+	printf("%d\n", r);
+
+	return (0);
+}
