059eca96e8c4a14e67d2921edbe0942d505a0795
brian-ikiara <brnikiara@gmail.com>
2022-11-08 09:02:20 +0000

    Heisenberg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,9 @@
  * On error, stderr.
  */
 
-void main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
