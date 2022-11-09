854f3e891a7c9f8de7543dce99e5c8042abcc564
brian-ikiara <brnikiara@gmail.com>
2022-11-09 11:48:03 +0000

    It's like strcat(), but better

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Function prototype
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Description: Similar to strcat(), but it uses malloc().
+ * Return: cs, concatenated string; NULL, if empty.
+ * On error, stderr.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0, j = 0, il = 0, jl = 0;
+	char *cs;
+
+	while (s1 && s1[il])
+		il++;
+	while (s2 && s2[jl])
+		jl++;
+
+	cs = malloc(sizeof(char) * (il + jl + 1));
+	if (cs == NULL)
+		return (NULL);
+
+	if (s1)
+	{
+		for (i; i < il; i++)
+			cs[i] = s1[i];
+	}
+
+	if (s2)
+	{
+		while (i < (il + jl))
+		{
+			cs[i] = s2[i];
+			i++;
+			j++;
+		}
+	}
+	cs[i] = '\0';
+
+	return (cs);
+}
