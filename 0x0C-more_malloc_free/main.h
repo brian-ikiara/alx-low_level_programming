cf5134aee455396dc0e344b619c783188b519993
brian-ikiara <brnikiara@gmail.com>
2022-11-14 14:49:48 +0000

    Confirms malloc()

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void *malloc_chacked(unsigned int);
+void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
