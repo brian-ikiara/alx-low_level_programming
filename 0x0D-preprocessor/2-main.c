f286005c0a20ca00dbdc5fe87f144ebf64690b73
brian-ikiara <brnikiara@gmail.com>
2022-11-15 13:10:48 +0000

    Say my name...

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Program's entry point
+ *
+ * Description: Prints the filename of program.
+ * Return: 0.
+ * On error, stderr.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
