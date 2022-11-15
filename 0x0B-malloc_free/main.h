9169a62104e0fd07e593e6e507df50bd02bdbe3e
brian-ikiara <brnikiara@gmail.com>
2022-11-15 13:03:12 +0000

    Few corrections

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_create_array(unsigned int, char);
+char *create_array(unsigned int, char);
 char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
