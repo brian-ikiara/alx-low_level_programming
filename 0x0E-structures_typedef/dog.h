20a4e3962159bf92ff19a051b12ec6f57ebf86b1
brian-ikiara <brnikiara@gmail.com>
2022-11-15 19:16:59 +0000

    Butter dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - Data structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Creates a dog's profile.
+ */
+
 struct dog
 {
 	char *name;
