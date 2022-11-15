cbd61f467260bf11f5b4d3cb8efff07ec417ad41
brian-ikiara <brnikiara@gmail.com>
2022-11-15 19:56:41 +0000

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,18 @@
  * Description: Creates a dog's profile.
  */
 
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
