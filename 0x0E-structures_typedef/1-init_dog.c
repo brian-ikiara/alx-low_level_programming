ba0af3a2d8754c19fc8c57a84c5ddf54e1f42494
brian-ikiara <brnikiara@gmail.com>
2022-11-15 19:33:23 +0000

    It's a dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * init_dog - Function prototype
+ * @d: The dog's profile
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Initializes a variable of type struct dog.
+ * Return: d, dog's profile.
+ * On error, stderr.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
