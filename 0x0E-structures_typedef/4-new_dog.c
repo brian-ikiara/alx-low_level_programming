99a3e81b73325381d852c31d06d841dbf22feed3
brian-ikiara <brnikiara@gmail.com>
2022-11-16 10:30:39 +0000

    It's a dog wid sum butter on 'im

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,19 +16,5 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
 
-	p = malloc(sizeof(dog_t));
-	if (*p == NULL)
-		return (NULL);
-	
-	(*p).name = name;
-	strcpy((*p).name);
-
-	(*p).age = age;
-
-	(*p).owner = owner;
-	strcpy((*p).owner);
-
-	return (p);
 }
