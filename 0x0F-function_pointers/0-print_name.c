99bf19d72be8b0d89433aedeef5713a291b6160f
brian-ikiara <brnikiara@gmail.com>
2022-11-17 18:46:03 +0000

    Fancy puts()

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,26 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
-
-/**
- * _ps - Function prototype
- * @s: String to be printed
- *
- * Description: Prints a string.
- * Return: void.
- * On error, stderr.
- */
-
-void _ps(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		putchar(s[i]);
-		i++;
-	}
-	putchar('\n');
-}
 
 /**
  * print_name - Function prototype
@@ -34,6 +12,5 @@ void _ps(char *s)
 
 void print_name(char *name, void (*f)(char *))
 {
-	f = &_ps;
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdio.h>
+
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
 
