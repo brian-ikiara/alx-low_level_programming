33a3e8e62bd6b74eb37038a00bb5ac960a0d4018
brian-ikiara <brnikiara@gmail.com>
2022-11-17 12:45:02 +0000

    Fancy puts()

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,38 @@
+#include "function_pointers.h"
+
+/**
+ * _ps - Function prototype
+ * @s: String to be printed
+ *
+ * Description: Prints a string.
+ * Return: void.
+ * On error, stderr.
+ */
+
+void _ps(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_name - Function prototype
+ * @name: Name to be printed
+ * @f: Pointer to print_name
+ *
+ * Description: Prints out a name given.
+ * Return: Nothing, since it's void.
+ * On error, stderr.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f = &_ps;
+	f(name);
+}
