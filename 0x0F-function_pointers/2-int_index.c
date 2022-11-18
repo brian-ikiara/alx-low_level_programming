b5197357b361d1749bf801990568f5519ef74ede
brian-ikiara <brnikiara@gmail.com>
2022-11-18 08:35:07 +0000

    Indices()

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function
+ * @array: Pointer to array
+ * @size: Array size
+ * @cmp: Pointer to function used to compare ints
+ *
+ * Description: Searches for and compares an int to another.
+ * Return: i, index of element that doesn't return 0; -1 if no
+ * element matches or if size <= 0.
+ * On error, stderr.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (size <= 0 && !cmp(array[i]))
+			return (-1);
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (cmp(array[i]));
+}
