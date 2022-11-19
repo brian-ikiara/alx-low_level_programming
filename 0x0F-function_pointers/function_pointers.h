9e2617c4db83ce729d38629bef16275388fdfe0c
brian-ikiara <brnikiara@gmail.com>
2022-11-19 19:04:34 +0000

    Indices

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,7 +5,7 @@
 
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *, size_t, void (*action)(int));
+int int_index(int *, int, int (*cmp)(int));
 
 #endif
