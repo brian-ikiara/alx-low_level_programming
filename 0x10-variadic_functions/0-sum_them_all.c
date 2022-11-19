561bad76767e23cc346b292d84ec7677eb71730f
brian-ikiara <brnikiara@gmail.com>
2022-11-19 02:37:47 +0000

    Nums()

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list a;
-	unsigned int i, j = 0;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
 		return (0);
+
 	va_start(a, n);
 	for (i = 0; i < n; i++)
-		j += va_arg(a, int);
+		sum += va_arg(a, int);
 	va_end(a);
 
-	return (j);
+	return (sum);
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Variadic function
+ * @separator: The separator of the list
+ * @n: Count of integers passed to function
+ *
+ * Description: Prints numbers.
+ * Return: void.
+ * On error, stderr.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i, j = 0;
+	va_list a;
+
+	va_start(a, n);
+	for (i = 0; i >= n; i++)
+	{
+		printf("%d", va_arg(a, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(a);
+}
