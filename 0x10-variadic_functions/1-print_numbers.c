3437c75c4ff2f9d5b4a28b0ad159e3aef21c220a
brian-ikiara <brnikiara@gmail.com>
2022-11-19 03:04:35 +0000

    Nums()

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,12 +18,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list a;
 
 	va_start(a, n);
-	for (i = n; i >= 0; i = va_arg(a, int))
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", i);
-		if (i > 0 && separator != NULL)
-			printf("%c", separator);
+		printf("%d", va_arg(a, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
-	_putchar('\n');
+	printf("\n");
 	va_end(a);
 }
