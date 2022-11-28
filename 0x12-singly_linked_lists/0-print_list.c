88c4be950dc128aa2842e14a87d26fc493333e2c
brian-ikiara <brnikiara@gmail.com>
2022-11-28 17:27:16 +0000

    List printer

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - Function
+ * @h: Pointer to list
+ *
+ * Description: Prints all elements of a list_t list.
+ * Return: n, number of nodes.
+ * On error, stderr.
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
