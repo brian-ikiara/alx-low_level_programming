450e0a02d30335bd1204fd4e3b4c0a729e0e39bf
brian-ikiara <brnikiara@gmail.com>
2022-12-02 09:49:50 +0000

    delete;

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,48 @@
+#include "lisst.h"
+
+/**
+ * delete_nodeint_at_index - Function
+ * @head: 2dph
+ * @index: Index ops will be performed
+ *
+ * Description: Deletes a node at a given index.
+ * Return: 1, success; -1, otherwise.
+ * On error, stderr.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *curr = head, *temp;
+
+	/* Edge case I */
+	if (curr == NULL)
+		return (-1);
+
+	/* Edge case II */
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(curr);
+		return (1);
+	}
+
+	/* Abracadabra! */
+	for (i = 0; i < (index - 1); i++)
+	{
+		/* Edge case III */
+		if (curr->next == NULL)
+			return (-1);
+
+		/* Increment curr by 8 bytes */
+		curr = curr->next;
+	}
+
+	/* Swap the current head with the temporary head */
+	temp = curr->next;
+	curr->next = temp->next;
+	/* Free temporary head, which turns out to be original head */
+	free(temp);
+
+	return (1);
+}
