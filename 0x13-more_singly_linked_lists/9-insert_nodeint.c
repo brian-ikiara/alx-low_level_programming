d004f46b2144ff63defff6e5ec6a3f97e6ebae66
brian-ikiara <brnikiara@gmail.com>
2022-12-02 09:27:38 +0000

    Insert @idx

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Function
+ * @head: 2dph
+ * @idx: Index to be added at
+ * @n: Initializer of member n of struct listint_s
+ *
+ * Description: Adds a new node to linked list at a given
+ * position idx.
+ * Return: new_node, address of new node; NULL, otherwise.
+ * On error, stderr.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	/* Counter, new node & temporary location */
+	unsigned int i;
+	listint_t *new_node = malloc(sizeof(listint_t)), *temp = head;
+
+	/* Edge case I */
+	if (new_node == NULL)
+		return (NULL);
+	/* Data is assigned, if false */
+	new_node->n = n;
+
+	/* Edge case II */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		for (i = 0; i < (idx - 1); i++)
+		{
+			temp = temp->next;
+			/* Edge case III */
+			if (temp == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+		}
+		/* Swap the value of new_node with temp */
+		new_node->next = temp->next;
+		/* Add new node to the index, finally */
+		temp->next = new_node;
+	}
+
+	/* mem_add new_node */
+	return (new_node);
+}
