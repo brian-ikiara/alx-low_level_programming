57ad0a8326ba0cd6c74f3c88e1e4ec1f088be703
brian-ikiara <brnikiara@gmail.com>
2022-12-02 10:13:22 +0000

    delete()

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	/* Abracadabra! */
-	while (i < (index - 1)
+	while (i < (index - 1))
 	{
 		/* Edge case III */
 		if (curr->next == NULL)
