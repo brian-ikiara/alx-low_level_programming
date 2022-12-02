a17893df7a21c41bb1493c14ffa6cab6cad320da
brian-ikiara <brnikiara@gmail.com>
2022-12-02 06:11:25 +0000

    Vox Pops

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,9 +27,5 @@ listint_t *add_nodeint_end(listint_t **, const int);
 void free_listint(listint_t *);
 void free_listint2(listint_t **);
 int pop_listint(listint_t **);
-listint_t *get_nodeint_at_index(listint_t *, unsigned int);
-int sum_listint(listint_t *);
-listint_t *insert_nodeint_at_index(listint **, unsigned int, int);
-int delete_nodeint_at_index(listint_t **, unsigned int);
 
 #endif
