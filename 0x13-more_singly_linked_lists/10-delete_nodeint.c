1a5b0cb82d949c0635ec2bfebc393e723d98fec2
brian-ikiara <brnikiara@gmail.com>
2022-12-02 10:14:55 +0000

    delete()

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *curr = head, *temp;
+	listint_t *curr = *head, *temp;
 
 	/* Edge case I */
 	if (curr == NULL)
