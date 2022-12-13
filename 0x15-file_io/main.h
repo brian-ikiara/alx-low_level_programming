2419687206606e1e005f5aaf42eed8634fb4f1e8
brian-ikiara <brnikiara@gmail.com>
2022-12-13 19:38:43 +0000

    Update 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,12 +7,13 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stddef.h>
 
 int _putchar(char);
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);
 char *_buffer(char *);
-void cls(int);
+void cls(ssize_t);
 
 #endif
