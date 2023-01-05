a997402b4ffc4a2a6bc3a66595def4042cc2f1fb
brian-ikiara <brnikiara@gmail.com>
2023-01-05 10:17:27 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,20 +12,18 @@
 
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j, k = 0;
 
-	if (n > 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(','), _putchar(' ');
 
 				k = i * j;
-
 				if (k <= 9)
 					_putchar(' ');
 				if (k <= 99)
@@ -34,12 +32,9 @@ void print_times_table(int n)
 				{
 					_putchar((k / 100) + '0');
 					_putchar((k / 10) % 10 + '0');
-				}
-				else if (k <= 99 && k >= 10)
-				{
+				} else if (k <= 99 && k >= 10)
 					_putchar((k / 10) + '0');
-				}
-				_putchar((k & 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			_putchar('\n');
 		}
